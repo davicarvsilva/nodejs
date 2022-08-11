c40a91876cdc7b789e334a22630adb1e3a0be6e9
davicarvsilva <davicarv13@gmail.com>
2022-08-11 11:12:19 -0300

    feat: file upload

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const http = require('http');
 const fs = require('fs');
+var formidable = require('formidable');
 
 http.createServer(function(req, res){
-	res.writeHead(200, {'Content-type':'text/html'});
-
-	fs.appendFile('url.txt', req.url, function(err){
-		if (err) throw err;
-		console.log('Saved!');
-	});
-
-	res.end();
+	if (req.url == '/fileupload') {
+    		var form = new formidable.IncomingForm();
+    		form.parse(req, function (err, fields, files) {
+      			var oldpath = files.filetoupload.filepath;
+      			var newpath = './' + files.filetoupload.originalFilename;
+      			fs.rename(oldpath, newpath, function (err) {
+        			if (err) throw err;
+        			res.write('File uploaded and moved!');
+        			res.end();
+      			});
+    		});
+  	} else {
+	fs.readFile('index.html', function(err, data){
+			res.writeHead(200, {'Content-type':'text/html'});
+			res.write(data);
+			res.end();
+		});
+	}
 }).listen('8080');
