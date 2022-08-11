f317ddd7ed64b8def0e916086cf9f664262a93d4
davicarvsilva <davicarv13@gmail.com>
2022-08-11 11:28:18 -0300

    feat: send email

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const http = require('http');
-const fs = require('fs');
-var formidable = require('formidable');
+var nodemailer = require('nodemailer');
 
-http.createServer(function(req, res){
-	if (req.url == '/fileupload') {
-    		var form = new formidable.IncomingForm();
-    		form.parse(req, function (err, fields, files) {
-      			var oldpath = files.filetoupload.filepath;
-      			var newpath = './' + files.filetoupload.originalFilename;
-      			fs.rename(oldpath, newpath, function (err) {
-        			if (err) throw err;
-        			res.write('File uploaded and moved!');
-        			res.end();
-      			});
-    		});
-  	} else {
-	fs.readFile('index.html', function(err, data){
-			res.writeHead(200, {'Content-type':'text/html'});
-			res.write(data);
-			res.end();
-		});
-	}
-}).listen('8080');
+var transporter = nodemailer.createTransport({
+  service: 'outlook',
+  auth: {
+    user: 'yourmail@outlook.com',
+    pass: 'pass'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@outlook.com',
+  to: 'destination',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
