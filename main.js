3dd0f974267685cd385a4b6a608f2c376e996d0d
davicarvsilva <davicarv13@gmail.com>
2022-08-11 10:39:32 -0300

    feat: create file append

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(req, res){
-	fs.readFile('index.html', function(err, data){
-		res.writeHead(200, {'Content-type':'text/html'});
-		res.write(data);
-		res.end();
+	res.writeHead(200, {'Content-type':'text/html'});
+
+	fs.appendFile('url.txt', req.url, function(err){
+		if (err) throw err;
+		console.log('Saved!');
 	});
+
+	res.end();
 }).listen('8080');
