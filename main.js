e39d0950b5b1497de65a229cfc8dae63ba1f591f
davicarvsilva <davicarv13@gmail.com>
2022-08-12 08:08:47 -0300

    feat: create table

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-	host: "localhost",
+  	host: "localhost",
   	user: "root",
-  	password: "root"
+  	password: "root",
+  	database: "testes"
 });
 
 con.connect(function(err) {
   	if (err) throw err;
   	console.log("Connected!");
-	con.query('CREATE DATABASE IF NOT EXISTS nodedbteste', function (err, result) {
-    		if (err) throw err;
-    		console.log("Database created.");
+  	var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  	con.query(sql, function (err, result) {
+    	if (err) throw err;
+    	console.log("Table created");
   	});
 });
